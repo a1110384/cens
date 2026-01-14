d4e539c3877d03ff1d5de5e47c8bd6188613d503
a111 <maxbenard8@gmail.com>
2026-01-14 11:53:19 -0800

    lower res

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ static bool running = false;
 #define CHUNK_SIZE 512
 #define SINE_LENGTH 1024
 
-#define resShift 3
+#define resShift 1
 const unsigned int res = 1 << resShift;
 #define oscAmt (128 << resShift)
 const short oscs2 = oscAmt * 2;
