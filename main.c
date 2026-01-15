7d9e2b596b1a0108651eab233568dcace27f28a9
a111 <maxbenard8@gmail.com>
2026-01-14 18:19:32 -0800

    upped res to test

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int SAMPLE_RATE = 44100;
 #define CHUNK_SIZE 512
 #define SINE_LENGTH 1024
 
-#define resShift 0
+#define resShift 3
 const unsigned int res = 1 << resShift;
 #define oscAmt (128 << resShift)
 const short oscs2 = oscAmt * 2;
